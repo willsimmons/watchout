7fe49f9418fdf073dd495f23ae10150e41b73020
cbweaver <cashbweaver@gmail.com>
2016-09-23 14:27:28 -0700

    adding player

--- client/watchout.js
+++ client/watchout.js
@@ -7,37 +7,80 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let asteroids = new Array(10);
-
 let svg = d3.select('svg');
 let width = +svg.attr('width');
 let height = +svg.attr('height');
-let g = svg.append('g');
+let asteroidBelt = svg.append('g').attr('id', 'asteroidBelt');
+let player = svg.append('g').attr('id', 'player');
+
+let asteroids = [];
+let numAsteroids = 10;
+for (var i = 0; i < numAsteroids; i++) {
+  asteroids.push({
+    name: 'asteroid'+i,
+    x: getRandomIntInclusive(1, width),
+    y: getRandomIntInclusive(1, height)
+  });
+}
+
+let asteroidTransition = d3.transition()
+  .duration(500)
+  .ease(d3.easeLinear);
+
+  /*
+let tweenFunction = function tweenFunction(endData) {
+  let asteroid = d3.select(this);
+  let startX = this.cx.baseVal.value;
+  let startY = this.cx.baseVal.value;
+  console.log(endData);
+  console.log(startX, startY);
+
+  return function (t) {
+    return asteroid.attr('cx', startX + ()t)
+      .attr('cy', startY + t);
+  }
+}
+*/
 
 let update = function update(data) {
-  let a = g.selectAll('circle').data(data);
+  let a = asteroidBelt.selectAll('circle').data(data);
 
   // Update
   //a.data(data);
-  a.attr('class', 'update');
+  a.attr('class', 'asteroid update');
+    //.transition(asteroidTransition);
 
   // Enter
   a.enter().append('circle')
-      .attr('class', 'enter')
+      .attr('class', 'asteroid enter')
+      .attr('cx', function (d) { return d.x; })
+      .attr('cy', function (d) { return d.y; })
       .attr('r', 15)
+      //.attr('r', 0)
+      //.transition().duration(500).attr('r', 15)
+      //.each(function (d, i) {
+        //d3.select(this).transition()
+          //.duration(500)
+          //.ease(d3.easeQuadIn)
+          //.attr('r', 15)
+      //})
     .merge(a)
-      .attr('cx', function (d) {
-        //let oldX = this.cx.baseVal.value;
-        //let newX = Math.random() *
-        return getRandomIntInclusive(1, width);
-      })
-      .attr('cy', function (d) {
-        //return this.cy.baseVal.value + 1;
-        return getRandomIntInclusive(1, height);
+      .each(function (d, i) {
+        d3.select(this).transition()
+          .duration(2000)
+          .ease(d3.easePolyInOut)
+          .attr('cx', function (d) {
+            return getRandomIntInclusive(1, width);
+          })
+          .attr('cy', function (d) {
+            return getRandomIntInclusive(1, height);
+          })
       });
 
   // Exit
   a.exit().remove();
+
+  //return a.transition().duration(400).tween('custom', tweenFunction);
 }
 
 update(asteroids);
@@ -45,5 +88,5 @@ update(asteroids);
 console.log(d3);
 d3.interval(() => {
   update(asteroids);
-}, 1000);
+}, 2000);
 
