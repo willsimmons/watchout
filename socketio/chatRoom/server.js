7a62bc6dd43c40bb36864606274bdb237cad0916
willsimmons <willsimmons@gmail.com>
2016-09-24 14:12:26 -0700

    added nodemon and morgan for reloading and logging

--- socketio/chatRoom/server.js
+++ socketio/chatRoom/server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const morgan = require('morgan');
 
+app.use(morgan('combined'));
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
